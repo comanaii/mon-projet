16709a27d5d3064ea7445ef0fb9e1eb8b7feeca1
comanaii <comanaii@outlook.fr>
2024-06-04 06:18:48 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -21,3 +21,23 @@ document.addEventListener("DOMContentLoaded", function() {
 
     setInterval(createBubble, 500);
 });
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Show the first tab by default
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("letter1").style.display = "block";
+    document.querySelector(".tab-link").classList.add("active");
+});
